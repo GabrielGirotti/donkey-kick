0fb8e31ebcc145fa071eb770bec61a97a0e26cea
Gabriel <girottigabriel@gmail.com>
2025-07-30 16:43:00 +0200

    semantic html in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,32 +3,39 @@ import { FaInstagram, FaSpotify, FaYoutube } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="bg-black text-orange-500 py-8 px-4 text-center  border-t border-zinc-800">
-      <div className="flex justify-center space-x-6 mb-4 text-2xl">
+      <h2 className="sr-only">Información del sitio y créditos</h2>
+      <nav
+        aria-label="Redes sociales de Donkey Kick"
+        className="flex justify-center space-x-6 mb-4 text-2xl"
+      >
         <a
           href="https://www.instagram.com/donkey_kick/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-orange-300 transition"
+          aria-label="Instagram de Donkey Kick"
         >
-          <FaInstagram />
+          <FaInstagram aria-hidden="true" />
         </a>
         <a
           href="https://open.spotify.com/intl-es/artist/0BxpcBBFlQgZKn4Ny03Jnp"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-orange-300 transition"
+          aria-label="Spotify de Donkey Kick"
         >
-          <FaSpotify />
+          <FaSpotify aria-hidden="true" />
         </a>
         <a
           href="https://www.youtube.com/@donkey_kick"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-orange-300 transition"
+          aria-label="Youtube de Donkey Kick"
         >
-          <FaYoutube />
+          <FaYoutube aria-hidden="true" />
         </a>
-      </div>
+      </nav>
 
       <p className="text-lg font-semibold mb-2">Donkey Kick</p>
       <p className="text-sm italic text-zinc-400 mb-4">
@@ -38,14 +45,19 @@ export default function Footer() {
         &copy; {new Date().getFullYear()} Donkey Kick. Todos los derechos
         reservados.
       </p>
-      <p className="text-xs text-zinc-500">Hecho por   <a
+
+      <p className="text-xs text-zinc-500">
+        Hecho por{" "}
+        <a
           href="https://gabrielgirotti.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-orange-300 transition"
+          aria-label="Portafolio de Gabba en Netlify"
         >
           Gabba
-        </a></p>
+        </a>
+      </p>
     </footer>
   );
 }
